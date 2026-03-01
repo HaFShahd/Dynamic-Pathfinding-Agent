47ae66b01fcf66f068a86a789f0494b7ff81c156
Hafsa Shahid <144857526+HaFShahd@users.noreply.github.com>
2026-03-01 16:03:22 +0500

    Implement Node class for pathfinding
    Add Node class for pathfinding with A* algorithm attributes.

--- Dynamic_Pathfinding.py
+++ Dynamic_Pathfinding.py
@@ -3,3 +3,18 @@ import random
 import time
 import heapq
 import math
+
+CELL_SIZE = 25
+
+
+class Node:
+    def __init__(self, r, c):
+        self.r = r
+        self.c = c
+        self.g = float('inf')
+        self.h = 0
+        self.f = float('inf')
+        self.parent = None
+
+    def __lt__(self, other):
+        return self.f < other.f
